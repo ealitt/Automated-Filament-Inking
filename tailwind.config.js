b62d0ae39b8d87e8a3a48e136fb7274d9f9dbb9c
ealitt <ealitt@gmail.com>
2022-04-23 13:13:26 -0400

    changes tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
+const purgecss = [
+  "@fullhuman/postcss-purgecss",
+  {
+    content: ["./components/**/*.js", "./pages/**/*.js"],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
   },
+];
+
+module.exports = {
   plugins: [
     "postcss-import",
     "tailwindcss",
-    "autoprefixer"
+    "autoprefixer",
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
   ],
-}
+};
